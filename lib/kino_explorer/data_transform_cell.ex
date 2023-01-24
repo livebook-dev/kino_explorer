1f082336b4951b532c04b6d3a9a4d81f6d72dffc
Cristine Guadelupe <cristineguadelupe@me.com>
2023-01-24 19:00:05 -0300

    DataFrame cell - Initial implementation (#14)
    * Data Frame Cell - initial implementation
    
    * Add operations
    
    * Multiple sorting
    
    * Remove operations
    
    * Groups
    
    * Layout adjustments
    
    * Ungroup
    
    * Layout adjustments
    
    * Titles
    
    * Input with icon
    
    * Layout adjustments - input icon
    
    * Pivot
    
    * Pivot adjustments
    
    * Filters by type
    
    * Validations for filters
    
    * Initial tests
    
    * Improvements for pivot
    
    * Layout adjustments
    
    * Removes group_by operation
    
    * Removes pivot_longer operation
    
    * Layout adjustments
    
    * Unavailable options
    
    * Updates for filters
    
    * Filter for dates
    
    * Filter for booleans
    
    * Alias for DataFrame and Series
    
    * Update deps
    
    * Applying suggestions
    
    * Queries api and export to var
    
    * Improves code generation
    
    * Ensures the required requires
    
    * Update test/kino_explorer/data_frame_cell_test.exs
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>
    
    * Update lib/assets/data_frame_cell/main.js
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>
    
    * Update lib/kino_explorer/data_frame_cell.ex
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>
    
    * Update lib/assets/data_frame_cell/main.js
    
    Co-authored-by: José Valim <jose.valim@gmail.com>
    
    * Applying suggestions
    
    * Update lib/kino_explorer/data_frame_cell.ex
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>
    
    * Update lib/kino_explorer/data_frame_cell.ex
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>
    
    * Rename files to DataTransform
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>
    Co-authored-by: José Valim <jose.valim@gmail.com>

---
+++ lib/kino_explorer/data_transform_cell.ex
@@ -0,0 +1,332 @@
+defmodule KinoExplorer.DataTransformCell do
+  @moduledoc false
+
+  use Kino.JS, assets_path: "lib/assets/data_transform_cell"
+  use Kino.JS.Live
+  use Kino.SmartCell, name: "Data transform"
+
+  alias Explorer.DataFrame
+
+  @as_atom ["direction", "type"]
+
+  @impl true
+  def init(attrs, ctx) do
+    root_fields = %{"data_frame" => attrs["data_frame"], "assign_to" => attrs["assign_to"]}
+    operations = attrs["operations"] || default_operations()
+
+    ctx =
+      assign(ctx,
+        root_fields: root_fields,
+        operations: operations,
+        data_frame_alias: Explorer.DataFrame,
+        data_options: [],
+        missing_require: nil
+      )
+
+    {:ok, ctx, reevaluate_on_change: true}
+  end
+
+  @impl true
+  def scan_binding(pid, binding, env) do
+    data_options =
+      for {key, val} <- binding,
+          is_struct(val, DataFrame),
+          do: %{variable: Atom.to_string(key), columns: DataFrame.dtypes(val)}
+
+    data_frame_alias = data_frame_alias(env)
+    missing_require = missing_require(env)
+    send(pid, {:scan_binding_result, data_options, data_frame_alias, missing_require})
+  end
+
+  @impl true
+  def handle_connect(ctx) do
+    payload = %{
+      root_fields: ctx.assigns.root_fields,
+      operations: ctx.assigns.operations,
+      data_options: ctx.assigns.data_options,
+      missing_require: ctx.assigns.missing_require
+    }
+
+    {:ok, payload, ctx}
+  end
+
+  @impl true
+  def handle_info({:scan_binding_result, data_options, data_frame_alias, missing_require}, ctx) do
+    ctx =
+      assign(ctx,
+        data_options: data_options,
+        data_frame_alias: data_frame_alias,
+        missing_require: missing_require
+      )
+
+    updated_fields =
+      case {ctx.assigns.root_fields["data_frame"], data_options} do
+        {nil, [%{variable: data_frame} | _]} -> updates_for_data_frame(data_frame)
+        _ -> %{}
+      end
+
+    ctx = if updated_fields == %{}, do: ctx, else: assign(ctx, updated_fields)
+
+    broadcast_event(ctx, "set_available_data", %{
+      "data_options" => data_options,
+      "fields" => updated_fields,
+      "require" => missing_require
+    })
+
+    {:noreply, ctx}
+  end
+
+  @impl true
+  def handle_event("update_field", %{"field" => "data_frame", "value" => value}, ctx) do
+    updated_fields = updates_for_data_frame(value)
+    ctx = assign(ctx, updated_fields)
+    broadcast_event(ctx, "update_data_frame", %{"fields" => updated_fields})
+    {:noreply, ctx}
+  end
+
+  def handle_event("update_field", %{"operation" => nil, "field" => field, "value" => value}, ctx) do
+    parsed_value = parse_value(field, value)
+    ctx = update(ctx, :root_fields, &Map.put(&1, field, parsed_value))
+    broadcast_event(ctx, "update_root", %{"fields" => %{field => parsed_value}})
+    {:noreply, ctx}
+  end
+
+  def handle_event("update_field", %{"operation" => "filters", "field" => "column"} = fields, ctx) do
+    {column, idx} = {fields["value"], fields["idx"]}
+    updated_filter = updates_for_filters(column, ctx)
+    updated_operation = List.replace_at(ctx.assigns.operations["filters"], idx, updated_filter)
+    updated_operations = %{ctx.assigns.operations | "filters" => updated_operation}
+    ctx = assign(ctx, operations: updated_operations)
+
+    broadcast_event(ctx, "update_operation", %{
+      "operation" => "filters",
+      "idx" => idx,
+      "fields" => updated_filter
+    })
+
+    {:noreply, ctx}
+  end
+
+  def handle_event("update_field", %{"operation" => operation} = fields, ctx) do
+    {field, value, idx} = {fields["field"], fields["value"], fields["idx"]}
+    parsed_value = parse_value(field, value)
+
+    updated_operation =
+      put_in(ctx.assigns.operations[operation], [Access.at(idx), field], parsed_value)
+
+    updated_operations = %{ctx.assigns.operations | operation => updated_operation}
+    ctx = assign(ctx, operations: updated_operations)
+
+    broadcast_event(ctx, "update_operation", %{
+      "operation" => operation,
+      "idx" => idx,
+      "fields" => %{field => parsed_value}
+    })
+
+    {:noreply, ctx}
+  end
+
+  def handle_event("add_operation", %{"operation" => operation}, ctx) do
+    new_operation = operation |> String.to_existing_atom() |> default_operation()
+    updated_operation = ctx.assigns.operations[operation] ++ [new_operation]
+    updated_operations = %{ctx.assigns.operations | operation => updated_operation}
+    ctx = assign(ctx, operations: updated_operations)
+    broadcast_event(ctx, "set_operations", %{operation => updated_operation})
+
+    {:noreply, ctx}
+  end
+
+  def handle_event("remove_operation", %{"operation" => operation, "idx" => idx}, ctx) do
+    updated_operation =
+      if idx, do: List.delete_at(ctx.assigns.operations[operation], idx), else: []
+
+    updated_operations = %{ctx.assigns.operations | operation => updated_operation}
+    ctx = assign(ctx, operations: updated_operations)
+    broadcast_event(ctx, "set_operations", %{operation => updated_operation})
+
+    {:noreply, ctx}
+  end
+
+  defp updates_for_data_frame(data_frame) do
+    %{
+      root_fields: %{"data_frame" => data_frame, "assign_to" => nil},
+      operations: default_operations()
+    }
+  end
+
+  defp updates_for_filters(column, ctx) do
+    df = ctx.assigns.root_fields["data_frame"]
+    data = ctx.assigns.data_options
+    type = Enum.find_value(data, &(&1.variable == df && Map.fetch!(&1.columns, column)))
+    %{"filter" => "==", "column" => column, "value" => nil, "type" => Atom.to_string(type)}
+  end
+
+  defp parse_value(_field, ""), do: nil
+  defp parse_value(_field, value), do: value
+
+  defp convert_field(field, nil), do: {String.to_atom(field), nil}
+  defp convert_field(field, ""), do: {String.to_atom(field), nil}
+
+  defp convert_field(field, value) when field in @as_atom do
+    {String.to_atom(field), String.to_atom(value)}
+  end
+
+  defp convert_field(field, value), do: {String.to_atom(field), value}
+
+  @impl true
+  def to_attrs(ctx) do
+    ctx.assigns.root_fields
+    |> Map.put("operations", ctx.assigns.operations)
+    |> Map.put("data_frame_alias", ctx.assigns.data_frame_alias)
+  end
+
+  @impl true
+  def to_source(attrs) do
+    attrs
+    |> to_quoted()
+    |> Kino.SmartCell.quoted_to_string()
+  end
+
+  defp to_quoted(%{"data_frame" => nil}) do
+    quote do
+    end
+  end
+
+  defp to_quoted(%{"data_frame" => df, "assign_to" => variable} = attrs) do
+    attrs = Map.new(attrs, fn {k, v} -> convert_field(k, v) end)
+
+    sorting_args =
+      for sort <- attrs.operations["sorting"],
+          sort = Map.new(sort, fn {k, v} -> convert_field(k, v) end),
+          sort.direction != nil and sort.sort_by != nil do
+        {sort.direction, quoted_column(sort.sort_by)}
+      end
+      |> then(fn args -> if args != [], do: [args] end)
+
+    sorting = [
+      %{
+        field: :sorting,
+        name: :arrange,
+        module: attrs.data_frame_alias,
+        args: sorting_args
+      }
+    ]
+
+    filters =
+      for filter <- attrs.operations["filters"],
+          filter = Map.new(filter, fn {k, v} -> convert_field(k, v) end) do
+        %{
+          field: :filter,
+          name: :filter,
+          module: attrs.data_frame_alias,
+          args: build_filter([filter.column, filter.filter, filter.value, filter.type])
+        }
+      end
+
+    pivot = [
+      %{
+        field: :pivot_wider,
+        name: :pivot_wider,
+        module: attrs.data_frame_alias,
+        args: build_pivot(attrs.operations["pivot_wider"])
+      }
+    ]
+
+    nodes = filters ++ pivot ++ sorting
+    root = build_root(df)
+    Enum.reduce(nodes, root, &apply_node/2) |> build_var(variable)
+  end
+
+  defp build_root(df) do
+    quote do
+      unquote(Macro.var(String.to_atom(df), nil))
+    end
+  end
+
+  defp build_var(acc, nil), do: acc
+
+  defp build_var(acc, var) do
+    quote do
+      unquote({String.to_atom(var), [], nil}) = unquote(acc)
+    end
+  end
+
+  defp build_filter([column, filter, value, type] = args) do
+    if Enum.all?(args, &(&1 != nil)), do: build_filter(column, filter, value, type)
+  end
+
+  defp build_filter(column, filter, value, type) do
+    [{String.to_atom(filter), [], [quoted_column(column), cast_filter_value(type, value)]}]
+  end
+
+  defp build_pivot([%{"names_from" => names, "values_from" => values}]) do
+    if names && values, do: [names, values]
+  end
+
+  defp build_pivot(_), do: nil
+
+  defp apply_node(%{args: nil}, acc), do: acc
+
+  defp apply_node(%{field: _field, name: function, module: data_frame, args: args}, acc) do
+    quote do
+      unquote(acc) |> unquote(data_frame).unquote(function)(unquote_splicing(args))
+    end
+  end
+
+  defp quoted_column(string) do
+    var = String.to_atom(string)
+
+    if Macro.classify_atom(var) == :identifier do
+      {String.to_atom(string), [], nil}
+    else
+      quote do
+        col(unquote(string))
+      end
+    end
+  end
+
+  defp default_operations() do
+    %{
+      "filters" => [default_operation(:filters)],
+      "sorting" => [default_operation(:sorting)],
+      "pivot_wider" => [default_operation(:pivot_wider)]
+    }
+  end
+
+  defp default_operation(:filters),
+    do: %{"filter" => "==", "column" => nil, "value" => nil, "type" => "string"}
+
+  defp default_operation(:sorting), do: %{"sort_by" => nil, "direction" => "asc"}
+  defp default_operation(:pivot_wider), do: %{"names_from" => nil, "values_from" => nil}
+
+  defp cast_filter_value(:boolean, value), do: String.to_atom(value)
+  defp cast_filter_value(:integer, value), do: String.to_integer(value)
+  defp cast_filter_value(:float, value), do: Float.parse(value) |> elem(0)
+  defp cast_filter_value(type, value) when type in [:date, :datetime], do: to_date(type, value)
+  defp cast_filter_value(_, value), do: value
+
+  defp to_date(:date, value) do
+    case Date.from_iso8601(value) do
+      {:ok, date} -> date
+      _ -> nil
+    end
+  end
+
+  defp to_date(:datetime, value) do
+    case DateTime.from_iso8601(value) do
+      {:ok, date, _} -> date
+      _ -> nil
+    end
+  end
+
+  defp data_frame_alias(%Macro.Env{aliases: aliases}) do
+    case List.keyfind(aliases, Explorer.DataFrame, 1) do
+      {data_frame_alias, _} -> data_frame_alias
+      nil -> Explorer.DataFrame
+    end
+  end
+
+  defp missing_require(%Macro.Env{requires: requires}) do
+    if Explorer.DataFrame not in requires, do: "require Explorer.DataFrame"
+  end
+end
---
+++ test/kino_explorer/data_transform_cell_test.exs
@@ -0,0 +1,216 @@
+defmodule KinoExplorer.DataTransformCellTest do
+  use ExUnit.Case, async: true
+
+  import Kino.Test
+
+  alias KinoExplorer.DataTransformCell
+
+  setup :configure_livebook_bridge
+
+  @root %{
+    "data_frame" => "people",
+    "assign_to" => nil,
+    "data_frame_alias" => Explorer.DataFrame
+  }
+
+  @operations %{
+    "filters" => [%{"column" => nil, "filter" => "==", "type" => "string", "value" => nil}],
+    "pivot_wider" => [%{"names_from" => nil, "values_from" => nil}],
+    "sorting" => [%{"direction" => "asc", "sort_by" => nil}]
+  }
+
+  test "returns no source when starting fresh with no data" do
+    {_kino, source} = start_smart_cell!(DataTransformCell, %{})
+
+    assert source == ""
+  end
+
+  test "finds Explorer DataFrames in binding and sends the data options to the client" do
+    {kino, _source} = start_smart_cell!(DataTransformCell, %{})
+
+    teams = teams_df()
+    people = people_df()
+    invalid_data = %{self() => [1, 2], :y => [1, 2]}
+
+    env = Code.env_for_eval([])
+    DataTransformCell.scan_binding(kino.pid, binding(), env)
+
+    data_options = [
+      %{columns: %{"id" => :integer, "name" => :string}, variable: "people"},
+      %{
+        columns: %{"hour" => :integer, "team" => :string, "weekday" => :string},
+        variable: "teams"
+      }
+    ]
+
+    assert_broadcast_event(kino, "set_available_data", %{"data_options" => ^data_options})
+  end
+
+  describe "code generation" do
+    test "source for a data frame without operations" do
+      attrs = build_attrs(%{})
+
+      assert DataTransformCell.to_source(attrs) == """
+             people\
+             """
+    end
+
+    test "source for a data frame with sorting" do
+      attrs = build_attrs(%{"sorting" => [%{"direction" => "asc", "sort_by" => "name"}]})
+
+      assert DataTransformCell.to_source(attrs) == """
+             people |> Explorer.DataFrame.arrange(asc: name)\
+             """
+    end
+
+    test "source for a data frame with multiple sorting" do
+      attrs =
+        build_attrs(%{
+          "sorting" => [
+            %{"direction" => "asc", "sort_by" => "name"},
+            %{"direction" => "desc", "sort_by" => "id"}
+          ]
+        })
+
+      assert DataTransformCell.to_source(attrs) == """
+             people |> Explorer.DataFrame.arrange(asc: name, desc: id)\
+             """
+    end
+
+    test "source for a data frame with filtering" do
+      attrs =
+        build_attrs(%{
+          "filters" => [
+            %{"column" => "name", "filter" => "==", "type" => "string", "value" => "Ana"}
+          ]
+        })
+
+      assert DataTransformCell.to_source(attrs) == """
+             people |> Explorer.DataFrame.filter(name == "Ana")\
+             """
+    end
+
+    test "source for a data frame with multiple filtering" do
+      attrs =
+        build_attrs(%{
+          "filters" => [
+            %{"column" => "name", "filter" => "==", "type" => "string", "value" => "Ana"},
+            %{"column" => "id", "filter" => "<", "type" => "integer", "value" => "2"}
+          ]
+        })
+
+      assert DataTransformCell.to_source(attrs) == """
+             people |> Explorer.DataFrame.filter(name == "Ana") |> Explorer.DataFrame.filter(id < 2)\
+             """
+    end
+
+    test "source for a data frame with columns with spaces" do
+      root = %{"data_frame" => "df", "assign_to" => "new_df"}
+
+      operations = %{
+        "sorting" => [
+          %{"direction" => "asc", "sort_by" => "full name"},
+          %{"direction" => "desc", "sort_by" => "id"}
+        ],
+        "filters" => [
+          %{"column" => "full name", "filter" => "==", "type" => "string", "value" => "Ana"},
+          %{"column" => "id", "filter" => "<", "type" => "integer", "value" => "2"}
+        ]
+      }
+
+      attrs = build_attrs(root, operations)
+
+      assert DataTransformCell.to_source(attrs) == """
+             new_df =
+               df
+               |> Explorer.DataFrame.filter(col("full name") == "Ana")
+               |> Explorer.DataFrame.filter(id < 2)
+               |> Explorer.DataFrame.arrange(asc: col("full name"), desc: id)\
+             """
+    end
+
+    test "source for a data frame with pivot wider" do
+      root = %{"data_frame" => "teams"}
+      operations = %{"pivot_wider" => [%{"names_from" => "weekdays", "values_from" => "hour"}]}
+      attrs = build_attrs(root, operations)
+
+      assert DataTransformCell.to_source(attrs) == """
+             teams |> Explorer.DataFrame.pivot_wider("weekdays", "hour")\
+             """
+    end
+
+    test "source with alias" do
+      root = %{"data_frame_alias" => DF}
+
+      operations = %{
+        "filters" => [
+          %{"column" => "name", "filter" => "==", "type" => "string", "value" => "Ana"},
+          %{"column" => "id", "filter" => "<", "type" => "integer", "value" => "2"}
+        ]
+      }
+
+      attrs = build_attrs(root, operations)
+
+      assert DataTransformCell.to_source(attrs) == """
+             people |> DF.filter(name == "Ana") |> DF.filter(id < 2)\
+             """
+    end
+
+    test "source with export to var and no operations" do
+      attrs = build_attrs(%{"assign_to" => "exported_df"}, %{})
+
+      assert DataTransformCell.to_source(attrs) == """
+             exported_df = people\
+             """
+    end
+
+    test "source with export to var" do
+      root = %{"data_frame_alias" => DF, "assign_to" => "exported_df"}
+
+      operations = %{
+        "filters" => [
+          %{"column" => "name", "filter" => "==", "type" => "string", "value" => "Ana"},
+          %{"column" => "id", "filter" => "<", "type" => "integer", "value" => "2"}
+        ]
+      }
+
+      attrs = build_attrs(root, operations)
+
+      assert DataTransformCell.to_source(attrs) == """
+             exported_df = people |> DF.filter(name == "Ana") |> DF.filter(id < 2)\
+             """
+    end
+  end
+
+  defp people_df() do
+    Explorer.DataFrame.new(%{
+      id: [3, 1, 2],
+      name: ["Amy Santiago", "Jake Peralta", "Terry Jeffords"]
+    })
+  end
+
+  defp teams_df() do
+    Explorer.DataFrame.new(
+      weekday: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      team: ["A", "B", "C", "A", "B", "C", "A", "B", "C", "A"],
+      hour: [10, 9, 10, 10, 11, 15, 14, 16, 14, 16]
+    )
+  end
+
+  defp build_attrs(root_attrs \\ %{}, operations_attrs) do
+    root_attrs = Map.merge(@root, root_attrs)
+    operations_attrs = Map.merge(@operations, operations_attrs)
+    Map.put(root_attrs, "operations", operations_attrs)
+  end
+end
