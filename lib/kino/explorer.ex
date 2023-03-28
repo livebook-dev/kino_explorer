9504393e0219b00e3339c1f1965a142a38d46c2f
Cristine Guadelupe <cristineguadelupe@me.com>
2023-03-28 17:33:35 +0700

    Groups info (#53)
    * Show if a column is in a group
    
    * Hides group information when there are no groups
    
    * Tests for group info

--- lib/kino/explorer.ex
+++ lib/kino/explorer.ex
@@ -36,11 +36,12 @@ defmodule Kino.Explorer do
 
   @impl true
   def init({df, name}) do
+    groups = df.groups
     df = DataFrame.ungroup(df)
     total_rows = DataFrame.n_rows(df)
     dtypes = DataFrame.dtypes(df)
     sample_data = df |> DataFrame.head(1) |> DataFrame.to_columns()
-    summaries = summaries(df)
+    summaries = summaries(df, groups)
 
     columns =
       for name <- df.names, dtype = Map.fetch!(dtypes, name) do
@@ -54,21 +55,21 @@ defmodule Kino.Explorer do
 
     info = %{name: name, features: [:pagination, :sorting]}
 
-    {:ok, info, %{df: df, total_rows: total_rows, columns: columns}}
+    {:ok, info, %{df: df, total_rows: total_rows, columns: columns, groups: groups}}
   end
 
   @impl true
   def get_data(rows_spec, state) do
-    {records, total_rows, summaries} = get_records(state.df, rows_spec)
+    {records, total_rows, summaries} = get_records(state, rows_spec)
     columns = Enum.map(state.columns, &%{&1 | summary: summaries[&1.key]})
     data = records_to_data(columns, records)
     {:ok, %{columns: columns, data: {:columns, data}, total_rows: total_rows}, state}
   end
 
-  defp get_records(df, rows_spec) do
+  defp get_records(%{df: df, groups: groups}, rows_spec) do
     df = order_by(df, rows_spec[:order])
     total_rows = DataFrame.n_rows(df)
-    summaries = if total_rows > 0, do: summaries(df)
+    summaries = if total_rows > 0, do: summaries(df, groups)
     df = DataFrame.slice(df, rows_spec.offset, rows_spec.limit)
     records = DataFrame.to_columns(df)
     {records, total_rows, summaries}
@@ -84,11 +85,13 @@ defmodule Kino.Explorer do
     Enum.map(columns, fn column -> Map.fetch!(records, column.key) |> Enum.map(&to_string/1) end)
   end
 
-  defp summaries(df) do
+  defp summaries(df, groups) do
     df_series = DataFrame.to_series(df)
+    has_groups = length(groups) > 0
 
     for {column, series} <- df_series,
         summary_type = summary_type(series),
+        grouped = (column in groups) |> to_string(),
         nulls = Series.nil_count(series) |> to_string(),
         into: %{} do
       if summary_type == :numeric do
@@ -96,15 +99,25 @@ defmodule Kino.Explorer do
         mean = if is_float(mean), do: Float.round(mean, 2) |> to_string(), else: to_string(mean)
         min = Series.min(series) |> to_string()
         max = Series.max(series) |> to_string()
-        {column, %{keys: ["min", "max", "mean", "nulls"], values: [min, max, mean, nulls]}}
+        keys = ["min", "max", "mean", "nulls"]
+        values = [min, max, mean, nulls]
+
+        keys = if has_groups, do: keys ++ ["grouped"], else: keys
+        values = if has_groups, do: values ++ [grouped], else: values
+
+        {column, %{keys: keys, values: values}}
       else
         %{"counts" => top_freq, "values" => top} = most_frequent(series)
         top_freq = top_freq |> List.first() |> to_string()
         top = List.first(top) || ""
         unique = count_unique(series)
+        keys = ["unique", "top", "top_freq", "nulls"]
+        values = [unique, top, top_freq, nulls]
 
-        {column,
-         %{keys: ["unique", "top", "top_freq", "nulls"], values: [unique, top, top_freq, nulls]}}
+        keys = if has_groups, do: keys ++ ["grouped"], else: keys
+        values = if has_groups, do: values ++ [grouped], else: values
+
+        {column, %{keys: keys, values: values}}
       end
     end
   end
