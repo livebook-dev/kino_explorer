d2d3c4a63bb3a930dde8d6b37b16e433acff238e
Cristine Guadelupe <cristineguadelupe@me.com>
2023-07-07 11:32:41 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule KinoExplorer.MixProject do
   defp deps do
     [
       {:kino, "~> 0.9.4"},
-      {:explorer, "~> 0.6.0"},
+      {:explorer, "~> 0.6.1"},
       {:rustler, "~> 0.29.0", optional: true},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
